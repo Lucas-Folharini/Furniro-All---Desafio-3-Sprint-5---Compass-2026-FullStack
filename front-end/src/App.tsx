9baa4c3ee723be9cadc291d0f8e4204e05c16e34
lucas-folharini <folharinilucas@mx2.unisc.br>
2026-08-21 21:29:40 -0300

    feat: add routes and protection and redirect logic

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,56 +1,44 @@
-import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+
 import { Home } from "./pages/Home";
 import { Shop } from "./pages/Shop";
-import { ProductDetail } from "./pages/ProductDetail";
 import { Cart } from "./pages/Cart";
+import { ProductDetail } from "./pages/ProductDetail";
 
-import { useCartStore } from "./store/useCartStore";
+import { Login } from "./pages/Login";
+import { Register } from "@pages/Register";
+// import { Checkout } from './pages/Checkout';
+// import { Contact } from './pages/Contact';
 
-function App() {
-  useEffect(() => {
-    const handleStorageChange = (e: StorageEvent) => {
-      if (e.key === "shopping-cart-storage") {
-        useCartStore.persist.rehydrate();
-      }
-    };
-
-    window.addEventListener("storage", handleStorageChange);
-
-    return () => {
-      window.removeEventListener("storage", handleStorageChange);
-    };
-  }, []);
-
+export default function App() {
   return (
-    <div className="min-h-screen flex flex-col font-poppins">
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-        containerStyle={{
-          top: 110,
-        }}
-      />
-
+    <>
       <Header />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
+      <main className="min-h-screen">
+        <Routes>
+          {/* ROTAS PÚBLICAS */}
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop/:id" element={<ProductDetail />} />{" "}
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register/>} />
 
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/shop/:category" element={<Shop />} />
 
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+          {/* ROTAS PROTEGIDAS */}
+          <Route element={<ProtectedRoute />}>
+            {/* <Route path="/checkout" element={<Checkout />} /> */}
+            {/* <Route path="/contact" element={<Contact />} /> */}
+          </Route>
+        </Routes>
+      </main>
 
       <Footer />
-    </div>
+    </>
   );
 }
-
-export default App;
